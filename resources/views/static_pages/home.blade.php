fe5e3f81903e652323493a7e4bb35c2106722a5e
a232152000 <a232152000@gmail.com>
2019-06-01 22:38:20 +0800

    初始化樣式

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,5 +1,15 @@
 @extends('layouts/default')
-
 @section('content')
-<h1>主頁</h1>
+    <div class="jumbotron">
+        <h1>Hello Laravel</h1>
+        <p class="lead">
+            你現在所看到的是 <a href="https://learnku.com/courses/laravel-essential-training">Laravel 入門教學</a> 的範例主頁。
+        </p>
+        <p>
+            一切，將重這裡開始。
+        </p>
+        <p>
+            <a class="btn btn-lg btn-success" href="#" role="button">現在註冊</a>
+        </p>
+    </div>
 @stop
